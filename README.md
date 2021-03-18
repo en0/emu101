898daced657beb6967dbbaedcfd60ebee780e126
Ian Laird <irlaird@gmail.com>
2021-03-18 01:52:38 -0600

    i have a plan.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# emu101
-learning how to write emulators
+# EMU101 - A python implementation
+
+Originally, this was intended to learn how to right emulators. Then I
+got carried away and designed an entirely fictitious system architecture.
+I am refering to this architecture as EMU101.
+
+I have desgined this processor as a block diagram and deduced an instruction
+set based on control signals that would be used to set Mux and ALU operations.
+To be clear, this thing doesn't exist except as an emulator.
+
--- emu101/cpu.py
+++ emu101/cpu.py
@@ -1,23 +1,14 @@
-from .typing import c_uint16, c_uint8, c_bool
+from .typing import c_uint16
 
 
 class CPU:
     def __init__(self):
-        # Address Register
-        self.a = c_uint8(0)
+        # Address Registers
+        self.ip = c_uint16(0xf000)
+        self.sp = c_uint16(0x01ff)
+        self.dr = c_uint16(0x0200)
 
         # Data Registers
-        self.r1 = c_uint8(0)
-        self.r2 = c_uint8(0)
-
-        # Control registers
-        self.pc = c_uint16(0xfffc)
-        self.sp = c_uint16(0x1000)
-
-        # Flags - I d ont know about hese
-        self.if = c_bool(False) # Interrupt Flag
-        self.cf = c_bool(False) # Carry Flag
-        self.zf = c_bool(False) # Zero Flag
-        self.of = c_bool(False) # overflow Flag
-        self.sf = c_bool(False) # Sign Flag
-        self.hf = c_bool(False) # Halt Flag
+        self.d0 = c_uint16(0)
+        self.d1 = c_uint16(0)
+        self.d2 = c_uint16(0)
